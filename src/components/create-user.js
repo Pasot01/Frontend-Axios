866bd89d19ea266d75e81d51406c5f5cb0980c06
Pasot01 <contactmyphone14@gmail.com>
2022-08-18 18:37:54 +0200

    Ajout de composants create-user et users -> dossier components

---
+++ src/components/create-user.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const createUser = () => {
+    return (
+        <div>
+            <p>Create User Component</p>
+        </div>
+    )
+};
+
+export default createUser;
---
+++ src/components/users.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const users = () => {
+    return (
+        <div>
+            <p>User Component</p>
+        </div>
+    )
+};
+
+export default users;
