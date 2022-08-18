ac198bfcd240d740dfd38961016393ee6b24fdcc
Pasot01 <contactmyphone14@gmail.com>
2022-08-18 18:09:34 +0200

    Configuration de bootstrap pour React

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./css/styles.css";
+
 function App() {
   return (
-    <div>
-      <h1>
-        React Application
+    <div className="container-fluid">
+      <h1 className="jumbotron text-center bg-primary">
+        React Axios Demo
       </h1>
     </div>
   );
