7955874cc4b637cc53ff4695d08b5f4b7424a870
Pasot01 <contactmyphone14@gmail.com>
2022-08-18 19:34:37 +0200

    Configuration du routage pour les composants React

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./css/styles.css";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+import Home from './components/home';
+import CreateUser from './components/create-user';
+import Users from './components/users';
 
 function App() {
   return (
-    <div className="container-fluid">
-      <h1 className="jumbotron text-center bg-primary">
-        React Axios Demo
-      </h1>
+  <BrowserRouter>
+    <div>
+      <header>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+          <a className="navbar-brand">React Axios Tutorial</a>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active">
+                <Link className="nav-link" to="/create-user">Create User</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/users">Users List</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/create-user' element={<CreateUser />} />
+              <Route path='/users' element={<Users />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
     </div>
+  </BrowserRouter>
   );
 }
 
